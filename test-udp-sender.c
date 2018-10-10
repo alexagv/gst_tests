17f4f07ec4f89d5bfaeb265c08526c2d02e7365c
Alexander G Vedeler <alexander.vedeler@blueye.no>
2018-10-10 09:45:28 +0200

    Clean up empty lines

--- test-udp-sender.c
+++ test-udp-sender.c
@@ -21,13 +21,10 @@ bus_call (GstBus     *bus,
     case GST_MESSAGE_ERROR: {
       gchar  *debug;
       GError *error;
-
       gst_message_parse_error (msg, &error, &debug);
       g_free (debug);
-
       g_printerr ("Error: %s\n", error->message);
       g_error_free (error);
-
       g_main_loop_quit (loop);
       break;
     }
